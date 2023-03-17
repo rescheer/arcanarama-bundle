bddb5aeb74cd5a9745fd2d7604229821c1f3192c
Robert Scheer <robertedwinscheer@gmail.com>
2023-03-17 17:19:57 -0500

    feat(osc): mute/unmute function

--- src/extension/modules/OSC.js
+++ src/extension/modules/OSC.js
@@ -1,6 +1,25 @@
 import * as OSC from 'osc';
+import { getContext } from '../util/nodecg-api-context';
 
 let udpPort;
+
+/*
+Channel assignments:
+01: DM
+02: Sam
+03: Guest (Josh?)
+04: [UNUSED]
+05: Taylor
+06: Kitty
+07: Music (Left)
+08: Music (Right)
+*/
+
+/*
+ * (Note copied from AGDQ OSC implementation)
+ * NOTE: It is absolutely critical that the `args` param of any udpPort.send command not be null or undefined.
+ * Doing so causes the osc lib to actually encode it as a null argument (,N). Instead, use an empty array ([]).
+ */
 
 export function initMixer(nodecg) {
   const coreStatus = nodecg.Replicant('coreStatus');
@@ -13,8 +32,10 @@ export function initMixer(nodecg) {
       localPort: 57121,
       remotePort: mixerPort,
       remoteAddress: mixerIp,
+      metadata: true,
     });
   } else {
+    // Mixer IP not in bundle
     nodecg.log.warn(
       '[OSC] Mixer IP not found in bundle config. Mixer functionality unavailable'
     );
@@ -25,9 +46,12 @@ export function initMixer(nodecg) {
   });
 
   udpPort.on('message', (oscMessage) => {
+    // Needs to check for responses and update the status rep
     if (oscMessage.address === '/info') {
-      nodecg.log.info('[OSC] Mixer connection successful');
+      nodecg.log.info('[OSC] Mixer connection test successful');
       coreStatus.value.mixerConnected = true;
+    } else {
+      nodecg.log.info(oscMessage);
     }
   });
 
@@ -37,5 +61,32 @@ export function initMixer(nodecg) {
 }
 
 export function sendTestMessage() {
-  udpPort.send({ address: '/info' });
+  udpPort.send({ address: '/info', args: [] });
+}
+
+export function setChannelMute(ch, bool) {
+  const nodecg = getContext();
+  let channel = ch;
+  const mutedState = bool ? 0 : 1;
+  const mutedString = bool ? 'mute' : 'unmute';
+
+  if (channel > 0 && channel <= 16) {
+    if (ch.toString().length === 1) {
+      channel = `0${ch}`;
+    }
+    udpPort.send({
+      address: `/ch/${channel}/mix/on`,
+      args: [{ type: 'i', value: mutedState }],
+    });
+
+    // Get
+    udpPort.send({
+      address: `/ch/${channel}/mix/on`,
+      args: [],
+    });
+  } else {
+    nodecg.log.warn(
+      `[OSC] Attempted to ${mutedString} channel out of bounds (channel ${channel})`
+    );
+  }
 }
