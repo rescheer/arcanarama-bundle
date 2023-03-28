f0772a57d744d2e1be28ae364952c0cb90a26964
Robert Scheer <robertedwinscheer@gmail.com>
2023-03-28 03:41:56 -0500

    feat(tracker): Notification system

---
+++ src/dashboard/apps/components/NotificationAction.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Icon from '@mui/material/Icon';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export function NotificationTimed(props) {
+  const { text, duration, closeSnackbar } = props;
+  const UPDATE_MS = 30;
+
+  const step = duration / (duration / UPDATE_MS);
+
+  const [progress, setProgress] = React.useState(duration * 0.95);
+  const normalize = (value) => Math.floor((value * 100) / duration);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress <= 0 ? 0 : prevProgress - step
+      );
+    }, UPDATE_MS);
+
+    if (progress <= 0) {
+      clearInterval(timer);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [duration, progress, step]);
+
+  const noteCloseAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      value={text}
+      onClick={(e) => closeSnackbar(e.currentTarget.value)}
+    >
+      <CircularProgress
+        variant="determinate"
+        color="inherit"
+        value={normalize(progress)}
+        sx={{ position: 'absolute' }}
+      />
+      <Icon fontSize="large">close</Icon>
+    </IconButton>
+  );
+
+  return noteCloseAction;
+}
+
+export function NotificationPersist(props) {
+  const { text, closeSnackbar } = props;
+
+  const noteCloseAction = (
+    <IconButton
+      size="small"
+      aria-label="close"
+      color="inherit"
+      value={text}
+      onClick={(e) => closeSnackbar(e.currentTarget.value)}
+    >
+      <Icon fontSize="large">close</Icon>
+    </IconButton>
+  );
+
+  return noteCloseAction;
+}
--- src/dashboard/tracker.jsx
+++ src/dashboard/tracker.jsx
@@ -1,20 +1,72 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useReplicant } from 'use-nodecg';
+import { SnackbarProvider, useSnackbar } from 'notistack';
 
 // Ours
 import TrackerApp from './apps/TrackerApp';
 import CharacterSelectApp from './apps/CharacterSelectApp';
 import AddCharacterApp from './apps/AddCharacterApp';
+import {
+  NotificationTimed,
+  NotificationPersist,
+} from './apps/components/NotificationAction';
 
 const charactersRep = window.nodecg.Replicant('characters');
 
 function App() {
   // eslint-disable-next-line no-unused-vars
   const [characters, updateCharacters] = useReplicant('characters');
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const [activeApp, setActiveApp] = React.useState('select');
   const [activeCharId, setActiveCharId] = React.useState('');
+
+  function handleNewNote(data) {
+    const NOTE_DEFAULT_VARIANT = 'info';
+    const noteVariant = data.variant || NOTE_DEFAULT_VARIANT;
+
+    let noteAction;
+    let noteDuration;
+    if (data.duration !== null) {
+      noteDuration = data.duration;
+      noteAction = (
+        <NotificationTimed
+          text={data.text}
+          duration={noteDuration}
+          closeSnackbar={(e) => closeSnackbar(e)}
+        />
+      );
+    } else {
+      noteDuration = null;
+      noteAction = (
+        <NotificationPersist
+          text={data.text}
+          closeSnackbar={(e) => closeSnackbar(e)}
+        />
+      );
+    }
+
+    enqueueSnackbar(data.text, {
+      key: data.text,
+      action: noteAction,
+      variant: noteVariant,
+      autoHideDuration: noteDuration,
+      disableWindowBlurListener: true,
+      preventDuplicate: true,
+      hideIconVariant: true,
+      style: { minWidth: '200px', left: '0px' },
+    });
+  }
+
+  React.useEffect(() => {
+    window.nodecg.listenFor('notifier', handleNewNote);
+    // Cleanup
+    return () => {
+      window.nodecg.unlisten('notifier', handleNewNote);
+    };
+  });
 
   if (characters) {
     let component;
@@ -51,7 +103,9 @@ function App() {
 window.NodeCG.waitForReplicants(charactersRep).then(() => {
   ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
-      <App />
+      <SnackbarProvider dense>
+        <App />
+      </SnackbarProvider>
     </React.StrictMode>
   );
 });
